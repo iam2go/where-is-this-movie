7bf40e62379492a14ae629ad752c917b67f39e4c
go <xxxviii00@gmail.com>
2022-11-09 10:47:34 +0900

    🔧 path alias 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GlobalStyle from "./styles/GlobalStyle";
+import GlobalStyle from "@styles/GlobalStyle";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
-import { theme } from "./styles/theme";
+import { theme } from "@styles/theme";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
